8a14ebdf1c00be919c95b893054af1744613fb46
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-04-25 17:43:51 +0900

    Added a method to coordinate_functions.

--- gl/coordinatetool/coordinate_functions.go
+++ gl/coordinatetool/coordinate_functions.go
@@ -1,10 +1,16 @@
 package coordinatetool
 
+import "math"
+
 func NormalizeCoordinate_Int(value int, max int) float32 {
 	return 2.0*float32(value)/float32(max) - 1.0
 }
 func NormalizeCoordinate_Float32(value float32, max float32) float32 {
 	return 2.0*value/max - 1.0
+}
+
+func ExpandNormalizedCoordinate(value float32, max int) int {
+	return int(math.Round(float64(float32(max) * (value + 1.0) * 0.5)))
 }
 
 func ConvertWindowCoordinateAndOpenGLCoordinate_Y(y int, height int) int {
