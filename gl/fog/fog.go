71d2754097d6e157db3706032164b20734c824bc
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-04-25 13:54:14 +0900

    Added fog.

---
+++ gl/fog/fog.go
@@ -0,0 +1,49 @@
+package fog
+
+import (
+	"github.com/dabasan/go-dh3dbasis/coloru8"
+	"github.com/dabasan/goglf/gl/shader"
+)
+
+type Fog struct {
+	start float32
+	end   float32
+	color coloru8.ColorU8
+
+	programs []*shader.ShaderProgram
+}
+
+func NewFog() *Fog {
+	fog := new(Fog)
+
+	fog.start = 100.0
+	fog.end = 200.0
+	fog.color = coloru8.GetColorU8FromFloat32Components(0.0, 0.0, 0.0, 1.0)
+
+	fog.programs = make([]*shader.ShaderProgram, 0)
+}
+
+func (f *Fog) AddProgram(program *shader.ShaderProgram) {
+	f.programs = append(f.programs, program)
+}
+func (f *Fog) RemoveAllPrograms() {
+	f.programs = make([]*shader.ShaderProgram, 0)
+}
+
+func (f *Fog) SetFogStartEnd(start float32, end float32) {
+	f.start = start
+	f.end = end
+}
+func (f *Fog) SetFogColor(color coloru8.ColorU8) {
+	f.color = color
+}
+
+func (f *Fog) Update() {
+	for _, program := range f.programs {
+		program.Enable()
+		program.SetUniform1f("fog_start", f.start)
+		program.SetUniform1f("fog_end", f.end)
+		program.SetUniformColorU8("fog_color", f.color)
+		program.Disable()
+	}
+}
