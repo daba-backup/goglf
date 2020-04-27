b5208f6e2a980a708e8db2c758cc0e2e96fead88
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-04-27 12:13:47 +0900

    Updated fps_animator.

--- gl/util/fps_animator.go
+++ gl/util/fps_animator.go
@@ -1,38 +1,24 @@
 package util
 
 import (
-	"github.com/go-gl/glfw/v3.3/glfw"
+	"math"
+	"time"
 )
 
 var fps float64
-
-var current_time float64
-var last_time float64
-var elapsed_time float64
+var spf float64
 
 func init() {
 	fps = 30.0
-
-	last_time = 0.0
-	elapsed_time = 0.0
+	spf = 1.0 / fps
 }
 
-func UpdateTimer() {
-	current_time = glfw.GetTime()
-	elapsed_time = current_time - last_time
-}
-
-func IsElapsed() bool {
-	if elapsed_time >= 1.0/fps {
-		last_time = current_time
-		elapsed_time = 0.0
-
-		return true
-	} else {
-		return false
-	}
+func Sleep() {
+	ms := time.Duration(math.Round(spf * 1000.0))
+	time.Sleep(ms * time.Millisecond)
 }
 
 func SetFPS(f float64) {
 	fps = f
+	spf = 1.0 / fps
 }
