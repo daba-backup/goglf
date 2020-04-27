b1c5baf936c763be5d39c3f2b8b9d73cd484cb69
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-04-27 22:26:19 +0900

    Added functions to gl_draw_functions_3d.

--- gl/draw/gl_draw_functions_3d.go
+++ gl/draw/gl_draw_functions_3d.go
@@ -8,6 +8,7 @@ import (
 	"github.com/dabasan/go-dh3dbasis/coloru8"
 	"github.com/dabasan/go-dh3dbasis/vector"
 	"github.com/dabasan/goglf/gl/shader"
+	"github.com/dabasan/goglf/gl/shape"
 	"github.com/dabasan/goglf/gl/wrapper"
 )
 
@@ -120,4 +121,153 @@ func DrawAxes_Negative(length float32) {
 		vector.VGet(0.0, 0.0, -length),
 		vector.VGet(0.0, 0.0, 0.0),
 		coloru8.GetColorU8FromFloat32Components(0.0, 0.0, 1.0, 1.0))
+}
+
+func DrawTriangle3D(triangle *shape.Triangle) {
+	var pos_vbo uint32
+	var color_vbo uint32
+	var vao uint32
+
+	pos_buffer := make([]float32, 9)
+	color_buffer := make([]float32, 12)
+	for i := 0; i < 3; i++ {
+		pos := triangle.Vertices[i].Pos
+		dif := triangle.Vertices[i].Dif
+
+		pos_buffer[i*3] = pos.X
+		pos_buffer[i*3+1] = pos.Y
+		pos_buffer[i*3+2] = pos.Z
+		color_buffer[i*4] = dif.R
+		color_buffer[i*4+1] = dif.G
+		color_buffer[i*4+2] = dif.B
+		color_buffer[i*4+3] = dif.A
+	}
+
+	shader.UseProgram("color")
+
+	wrapper.GenBuffers(1, &pos_vbo)
+	wrapper.GenBuffers(1, &color_vbo)
+
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, pos_vbo)
+	wrapper.BufferData(gl.ARRAY_BUFFER,
+		wrapper.SIZEOF_FLOAT*len(pos_buffer), unsafe.Pointer(&pos_buffer[0]), gl.STATIC_DRAW)
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, color_vbo)
+	wrapper.BufferData(gl.ARRAY_BUFFER,
+		wrapper.SIZEOF_FLOAT*len(color_buffer), unsafe.Pointer(&color_buffer[0]), gl.STATIC_DRAW)
+
+	wrapper.GenVertexArrays(1, &vao)
+	wrapper.BindVertexArray(vao)
+
+	//Position attribute
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, pos_vbo)
+	wrapper.EnableVertexAttribArray(0)
+	wrapper.VertexAttribPointer(0, 3, gl.FLOAT, false, wrapper.SIZEOF_FLOAT*3, nil)
+
+	//Color attribute
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, color_vbo)
+	wrapper.EnableVertexAttribArray(1)
+	wrapper.VertexAttribPointer(1, 4, gl.FLOAT, false, wrapper.SIZEOF_FLOAT*4, nil)
+
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, 0)
+	wrapper.BindVertexArray(0)
+
+	//Draw
+	wrapper.BindVertexArray(vao)
+	wrapper.Enable(gl.BLEND)
+	wrapper.DrawArrays(gl.LINE_LOOP, 0, 3)
+	wrapper.Disable(gl.BLEND)
+	wrapper.BindVertexArray(0)
+
+	//Delete buffers
+	wrapper.DeleteBuffers(1, &pos_vbo)
+	wrapper.DeleteBuffers(1, &color_vbo)
+	wrapper.DeleteVertexArrays(1, &vao)
+}
+func DrawTriangle3D_Vertices(pos_1 vector.Vector, pos_2 vector.Vector, pos_3 vector.Vector, color coloru8.ColorU8) {
+	var triangle shape.Triangle
+
+	triangle.Vertices[0].Pos = pos_1
+	triangle.Vertices[1].Pos = pos_2
+	triangle.Vertices[2].Pos = pos_3
+	for i := 0; i < 3; i++ {
+		triangle.Vertices[i].Dif = color
+	}
+
+	DrawTriangle3D(&triangle)
+}
+func DrawQuadrangle3D(quadrangle *shape.Quadrangle) {
+	var pos_vbo uint32
+	var color_vbo uint32
+	var vao uint32
+
+	pos_buffer := make([]float32, 12)
+	color_buffer := make([]float32, 16)
+	for i := 0; i < 4; i++ {
+		pos := quadrangle.Vertices[i].Pos
+		dif := quadrangle.Vertices[i].Dif
+
+		pos_buffer[i*3] = pos.X
+		pos_buffer[i*3+1] = pos.Y
+		pos_buffer[i*3+2] = pos.Z
+		color_buffer[i*4] = dif.R
+		color_buffer[i*4+1] = dif.G
+		color_buffer[i*4+2] = dif.B
+		color_buffer[i*4+3] = dif.A
+	}
+
+	shader.UseProgram("color")
+
+	wrapper.GenBuffers(1, &pos_vbo)
+	wrapper.GenBuffers(1, &color_vbo)
+
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, pos_vbo)
+	wrapper.BufferData(gl.ARRAY_BUFFER,
+		wrapper.SIZEOF_FLOAT*len(pos_buffer), unsafe.Pointer(&pos_buffer[0]), gl.STATIC_DRAW)
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, color_vbo)
+	wrapper.BufferData(gl.ARRAY_BUFFER,
+		wrapper.SIZEOF_FLOAT*len(color_buffer), unsafe.Pointer(&color_buffer[0]), gl.STATIC_DRAW)
+
+	wrapper.GenVertexArrays(1, &vao)
+	wrapper.BindVertexArray(vao)
+
+	//Position attribute
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, pos_vbo)
+	wrapper.EnableVertexAttribArray(0)
+	wrapper.VertexAttribPointer(0, 3, gl.FLOAT, false, wrapper.SIZEOF_FLOAT*3, nil)
+
+	//Color attribute
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, color_vbo)
+	wrapper.EnableVertexAttribArray(1)
+	wrapper.VertexAttribPointer(1, 4, gl.FLOAT, false, wrapper.SIZEOF_FLOAT*4, nil)
+
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, 0)
+	wrapper.BindVertexArray(0)
+
+	//Draw
+	wrapper.BindVertexArray(vao)
+	wrapper.Enable(gl.BLEND)
+	wrapper.DrawArrays(gl.LINE_LOOP, 0, 4)
+	wrapper.Disable(gl.BLEND)
+	wrapper.BindVertexArray(0)
+
+	//Delete buffers
+	wrapper.DeleteBuffers(1, &pos_vbo)
+	wrapper.DeleteBuffers(1, &color_vbo)
+	wrapper.DeleteVertexArrays(1, &vao)
+}
+func DrawQuadrangle3D_Vertices(
+	pos_1 vector.Vector, pos_2 vector.Vector,
+	pos_3 vector.Vector, pos_4 vector.Vector, color coloru8.ColorU8) {
+	var quadrangle shape.Quadrangle
+
+	quadrangle.Vertices[0].Pos = pos_1
+	quadrangle.Vertices[1].Pos = pos_2
+	quadrangle.Vertices[2].Pos = pos_3
+	quadrangle.Vertices[3].Pos = pos_4
+
+	for i := 0; i < 4; i++ {
+		quadrangle.Vertices[i].Dif = color
+	}
+
+	DrawQuadrangle3D(&quadrangle)
 }
