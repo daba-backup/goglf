478fa674d23b99a26d7cd0083c259dd9ea4d436e
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-04-30 23:42:45 +0900

    Updated texture_mgr.

--- gl/texture/texture_mgr.go
+++ gl/texture/texture_mgr.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"unsafe"
 
+	"github.com/dabasan/goglf/gl/shader"
+
 	"github.com/dabasan/goglf/gl/common"
 
 	"github.com/dabasan/goglf/gl/wrapper"
@@ -19,6 +21,8 @@ type Texture struct {
 var textures_map map[int]*Texture
 var count int
 
+var default_texture_handle int
+
 var generate_mipmap_flag bool
 
 var window_width int
@@ -27,6 +31,8 @@ var window_height int
 func init() {
 	textures_map = make(map[int]*Texture)
 	count = 0
+
+	default_texture_handle = LoadTexture("./Data/Texture/white.bmp")
 
 	generate_mipmap_flag = true
 
@@ -237,8 +243,7 @@ func SetWindowSize(width int, height int) {
 func BindTexture(texture_handle int) int {
 	texture, ok := textures_map[texture_handle]
 	if !ok {
-		log.Printf("warn: No such texture. texture_handle=%v", texture_handle)
-		return -1
+		texture, _ = textures_map[default_texture_handle]
 	}
 
 	wrapper.BindTexture(gl.TEXTURE_2D, texture.object)
@@ -257,11 +262,10 @@ func DrawTexture(
 	top_left_u float32, top_left_v float32) int {
 	texture, ok := textures_map[texture_handle]
 	if !ok {
-		log.Printf("warn: No such texture. texture_handle=%v", texture_handle)
-		return -1
+		texture, _ = textures_map[default_texture_handle]
 	}
 
-	indices := make([]int, 6)
+	indices := make([]int32, 6)
 	pos_buffer := make([]float32, 8)
 	uv_buffer := make([]float32, 8)
 
@@ -277,5 +281,78 @@ func DrawTexture(
 	normalized_width := float32(width) / float32(window_width) * 2.0
 	normalized_height := float32(height) / float32(window_height) * 2.0
 
+	//Bottom left
+	pos_buffer[0] = normalized_x
+	pos_buffer[1] = normalized_y
+	uv_buffer[0] = bottom_left_u
+	uv_buffer[1] = bottom_left_v
+	//Bottom right
+	pos_buffer[2] = normalized_x + normalized_width
+	pos_buffer[3] = normalized_y
+	uv_buffer[2] = top_right_u
+	uv_buffer[3] = top_right_v
+	//Top right
+	pos_buffer[4] = normalized_x + normalized_width
+	pos_buffer[5] = normalized_y + normalized_height
+	uv_buffer[4] = top_right_u
+	uv_buffer[5] = top_right_v
+	//Top left
+	pos_buffer[6] = normalized_x
+	pos_buffer[7] = normalized_y + normalized_height
+	uv_buffer[6] = top_left_u
+	uv_buffer[7] = top_left_v
+
+	shader.UseProgram("texture_drawer")
+
+	var indices_vbo uint32
+	var pos_vbo uint32
+	var uv_vbo uint32
+	var vao uint32
+
+	wrapper.GenBuffers(1, &indices_vbo)
+	wrapper.GenBuffers(1, &pos_vbo)
+	wrapper.GenBuffers(1, &uv_vbo)
+	wrapper.GenVertexArrays(1, &vao)
+
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, pos_vbo)
+	wrapper.BufferData(gl.ARRAY_BUFFER,
+		wrapper.SIZEOF_FLOAT*len(pos_buffer), unsafe.Pointer(&pos_buffer[0]), gl.STATIC_DRAW)
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, uv_vbo)
+	wrapper.BufferData(gl.ARRAY_BUFFER,
+		wrapper.SIZEOF_FLOAT*len(uv_buffer), unsafe.Pointer(&uv_buffer[0]), gl.STATIC_DRAW)
+
+	wrapper.BindVertexArray(vao)
+
+	wrapper.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, indices_vbo)
+	wrapper.BufferData(gl.ELEMENT_ARRAY_BUFFER,
+		wrapper.SIZEOF_INT*len(indices), unsafe.Pointer(&indices[0]), gl.STATIC_DRAW)
+
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, pos_vbo)
+	wrapper.EnableVertexAttribArray(0)
+	wrapper.VertexAttribPointer(0, 2, gl.FLOAT, false, wrapper.SIZEOF_FLOAT*2, nil)
+
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, uv_vbo)
+	wrapper.EnableVertexAttribArray(1)
+	wrapper.VertexAttribPointer(1, 2, gl.FLOAT, false, wrapper.SIZEOF_FLOAT*2, nil)
+
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, 0)
+
+	wrapper.BindTexture(gl.TEXTURE_2D, texture.object)
+	wrapper.Enable(gl.BLEND)
+	wrapper.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, nil)
+	wrapper.Disable(gl.BLEND)
+	wrapper.BindTexture(gl.TEXTURE_2D, 0)
+
+	wrapper.BindVertexArray(0)
+
+	wrapper.DeleteBuffers(1, &indices_vbo)
+	wrapper.DeleteBuffers(1, &pos_vbo)
+	wrapper.DeleteBuffers(1, &uv_vbo)
+	wrapper.DeleteVertexArrays(1, &vao)
+
 	return 0
+}
+func DrawTexture_Simple(texture_handle int, x int, y int, width int, height int) int {
+	ret := DrawTexture(texture_handle, x, y, width, height, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0)
+	return ret
 }
