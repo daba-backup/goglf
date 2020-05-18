fbb582a479f44d2548274a5f84de9eeb9b28ca96
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-05-18 21:55:08 +0900

    Added a function to draw_functions_2d.

--- gl/draw/draw_functions_2d.go
+++ gl/draw/draw_functions_2d.go
@@ -3,12 +3,14 @@ package draw
 import (
 	"unsafe"
 
+	"github.com/dabasan/go-dh3dbasis/coloru8"
+
 	"github.com/dabasan/goglf/gl/common"
-
-	"github.com/go-gl/gl/all-core/gl"
-
+	"github.com/dabasan/goglf/gl/coordinatetool"
 	"github.com/dabasan/goglf/gl/shader"
 	"github.com/dabasan/goglf/gl/wrapper"
+
+	"github.com/go-gl/gl/all-core/gl"
 )
 
 var simple_2d_program *shader.ShaderProgram
@@ -48,6 +50,74 @@ func SetTextureDrawerZ(z float32) {
 func SetWindowSize(width int, height int) {
 	window_width = width
 	window_height = height
+}
+
+func DrawLine2D(x1 int, y1 int, x2 int, y2 int, color coloru8.ColorU8) {
+	var pos_vbo uint32
+	var color_vbo uint32
+	var vao uint32
+
+	pos_buffer := make([]float32, 4)
+	color_buffer := make([]float32, 8)
+
+	normalized_x1 := coordinatetool.NormalizeCoordinate_Int(x1, window_width)
+	normalized_y1 := coordinatetool.NormalizeCoordinate_Int(y1, window_height)
+	normalized_x2 := coordinatetool.NormalizeCoordinate_Int(x2, window_width)
+	normalized_y2 := coordinatetool.NormalizeCoordinate_Int(y2, window_height)
+
+	pos_buffer[0] = normalized_x1
+	pos_buffer[1] = normalized_y1
+	pos_buffer[2] = normalized_x2
+	pos_buffer[3] = normalized_y2
+
+	color_buffer[0] = color.R
+	color_buffer[1] = color.G
+	color_buffer[2] = color.B
+	color_buffer[3] = color.A
+	color_buffer[4] = color.R
+	color_buffer[5] = color.G
+	color_buffer[6] = color.B
+	color_buffer[7] = color.A
+
+	wrapper.GenBuffers(1, &pos_vbo)
+	wrapper.GenBuffers(1, &color_vbo)
+
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, pos_vbo)
+	wrapper.BufferData(gl.ARRAY_BUFFER,
+		wrapper.SIZEOF_FLOAT*len(pos_buffer), unsafe.Pointer(&pos_buffer[0]), gl.STATIC_DRAW)
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, color_vbo)
+	wrapper.BufferData(gl.ARRAY_BUFFER,
+		wrapper.SIZEOF_FLOAT*len(color_buffer), unsafe.Pointer(&color_buffer[0]), gl.STATIC_DRAW)
+
+	wrapper.GenVertexArrays(1, &vao)
+	wrapper.BindVertexArray(vao)
+
+	//Position attribute
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, pos_vbo)
+	wrapper.EnableVertexAttribArray(0)
+	wrapper.VertexAttribPointer(0, 2, gl.FLOAT, false, wrapper.SIZEOF_FLOAT*2, nil)
+
+	//Color attribute
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, color_vbo)
+	wrapper.EnableVertexAttribArray(1)
+	wrapper.VertexAttribPointer(1, 4, gl.FLOAT, false, wrapper.SIZEOF_FLOAT*4, nil)
+
+	wrapper.BindBuffer(gl.ARRAY_BUFFER, 0)
+	wrapper.BindVertexArray(0)
+
+	//Draw
+	wrapper.BindVertexArray(vao)
+	wrapper.Enable(gl.BLEND)
+	simple_2d_program.Enable()
+	wrapper.DrawArrays(gl.LINES, 0, 2)
+	simple_2d_program.Disable()
+	wrapper.Disable(gl.BLEND)
+	wrapper.BindVertexArray(0)
+
+	//Delete buffers
+	wrapper.DeleteBuffers(1, &pos_vbo)
+	wrapper.DeleteBuffers(1, &color_vbo)
+	wrapper.DeleteVertexArrays(1, &vao)
 }
 
 func DrawTexture(
